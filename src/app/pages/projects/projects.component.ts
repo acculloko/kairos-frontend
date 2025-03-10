0bfaca5496506c9bec96c28a80613b3ace740677
Lucca Lucchin de Campos Costa <lucca.lucchin@gmail.com>
2025-03-09 21:44:40 -0300

    Project sort, filter and pagination

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -5,10 +5,11 @@ import {
   inject,
   NgZone,
   OnInit,
+  ViewChild,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ProjectService } from '../../services/project/project.service';
 import { Project } from '../../models/project/project.type';
 import { CommonModule } from '@angular/common';
@@ -16,10 +17,26 @@ import { Router } from '@angular/router';
 import { ProjectCreationFormComponent } from '../../components/project-creation-form/project-creation-form.component';
 import { ProjectCreationRequest } from '../../models/project/projectCreationRequest.type';
 import { format } from 'date-fns';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
-  imports: [MatTableModule, MatButtonModule, CommonModule],
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    CommonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+  ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
@@ -31,9 +48,10 @@ export class ProjectsComponent implements OnInit {
   dateService = inject(DateService);
   ngZone = inject(NgZone);
 
-  projectList: Array<Project> = [];
-
   // MatTable
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   displayedColumns: string[] = [
     'id',
     'name',
@@ -45,48 +63,169 @@ export class ProjectsComponent implements OnInit {
     'actions',
   ];
 
+  selectedFilterField: string = 'name';
+  filterableFields = [
+    { label: 'Id', value: 'id' },
+    { label: 'Name', value: 'name' },
+    { label: 'Start Date', value: 'start_date' },
+    { label: 'End Date', value: 'end_date' },
+    { label: 'Status', value: 'status' },
+    { label: 'Priority', value: 'priority' },
+    { label: 'Responsible User', value: 'responsible_user.name' },
+  ];
+
+  projectList: Array<Project> = [];
+  projectDataSource = new MatTableDataSource<Project>();
+
   ngOnInit(): void {
     this.getAllProjects();
   }
 
+  // getAllProjects() {
+  //   this.projectService.getProjects().subscribe((res: any) => {
+  //     this.projectList = res.map((project: Project) => ({
+  //       ...project,
+  //       start_date: this.dateService.parseDate(
+  //         project.start_date,
+  //         'dd/MM/yyyy'
+  //       ),
+  //       end_date: this.dateService.parseDate(project.end_date, 'dd/MM/yyyy'),
+  //       creation_date: this.dateService.parseDate(
+  //         project.creation_date,
+  //         'dd/MM/yyyy HH:mm:ss'
+  //       ),
+  //       responsible_user: {
+  //         ...project.responsible_user,
+  //         creation_date: this.dateService.parseDate(
+  //           project.responsible_user.creation_date,
+  //           'dd/MM/yyyy HH:mm:ss'
+  //         ),
+  //         last_login: project.responsible_user.last_login
+  //           ? this.dateService.parseDate(
+  //               project.responsible_user.last_login,
+  //               'dd/MM/yyyy HH:mm:ss'
+  //             )
+  //           : null,
+  //       },
+  //     }));
+  //   });
+  // }
+
   getAllProjects() {
-    this.projectService.getProjects().subscribe((res: any) => {
-      this.projectList = res.map((project: Project) => ({
-        ...project,
-        start_date: this.dateService.parseDate(
-          project.start_date,
-          'dd/MM/yyyy'
-        ),
-        end_date: this.dateService.parseDate(project.end_date, 'dd/MM/yyyy'),
-        creation_date: this.dateService.parseDate(
-          project.creation_date,
-          'dd/MM/yyyy HH:mm:ss'
-        ),
-        responsible_user: {
-          ...project.responsible_user,
+    this.projectService.getProjects().subscribe({
+      next: (response: any) => {
+        this.projectList = response.map((project: Project) => ({
+          ...project,
+          start_date: this.dateService.parseDate(
+            project.start_date,
+            'dd/MM/yyyy'
+          ),
+          end_date: this.dateService.parseDate(project.end_date, 'dd/MM/yyyy'),
           creation_date: this.dateService.parseDate(
-            project.responsible_user.creation_date,
+            project.creation_date,
             'dd/MM/yyyy HH:mm:ss'
           ),
-          last_login: project.responsible_user.last_login
-            ? this.dateService.parseDate(
-                project.responsible_user.last_login,
-                'dd/MM/yyyy HH:mm:ss'
-              )
-            : null,
-        },
-      }));
+          responsible_user: {
+            ...project.responsible_user,
+            creation_date: this.dateService.parseDate(
+              project.responsible_user.creation_date,
+              'dd/MM/yyyy HH:mm:ss'
+            ),
+            last_login: project.responsible_user.last_login
+              ? this.dateService.parseDate(
+                  project.responsible_user.last_login,
+                  'dd/MM/yyyy HH:mm:ss'
+                )
+              : null,
+          },
+        }));
+
+        this.projectDataSource = new MatTableDataSource<Project>(
+          this.projectList
+        );
+        console.log(this.projectDataSource);
+      },
+      error: (err) => {
+        console.error(err);
+        throw err;
+      },
+      complete: () => {
+        console.log('complete');
+        this.projectDataSource.paginator = this.paginator;
+        this.projectDataSource.sort = this.sort;
+        this.setupFilterPredicate();
+        this.setupSorting();
+      },
     });
+  }
+
+  onSearch(event: Event) {
+    const searchValue = (event.target as HTMLInputElement).value;
+    this.projectDataSource.filter = searchValue.trim().toLowerCase();
+  }
+
+  setupFilterPredicate() {
+    this.projectDataSource.filterPredicate = (
+      data: Project,
+      filter: string
+    ) => {
+      if (!this.selectedFilterField || !filter) return true;
+
+      let fieldValue: string = '';
+
+      if (this.selectedFilterField === 'id') {
+        fieldValue = data.id.toString();
+      } else if (this.selectedFilterField === 'start_date' && data.start_date) {
+        fieldValue =
+          this.dateService.formatDate(data.start_date, 'dd/MM/yyyy') || '';
+      } else if (this.selectedFilterField === 'end_date' && data.end_date) {
+        fieldValue =
+          this.dateService.formatDate(data.end_date, 'dd/MM/yyyy') || '';
+      } else {
+        fieldValue = (data as any)[this.selectedFilterField] || '';
+      }
+
+      return fieldValue.toLowerCase().includes(filter.toLowerCase());
+    };
+  }
+
+  setupSorting() {
+    const priorityOrder: Record<string, number> = {
+      HIGH: 1,
+      MEDIUM: 2,
+      LOW: 3,
+    };
+
+    this.projectDataSource.sortingDataAccessor = (
+      item: Project,
+      property: string
+    ): string | number => {
+      if (property === 'priority') {
+        return priorityOrder[item.priority] ?? 999; // Default large number if priority is unknown
+      }
+
+      if (property === 'start_date' || property === 'end_date') {
+        return item[property as keyof Project]
+          ? new Date(item[property as keyof Project] as string).getTime()
+          : 0;
+      }
+
+      if (property === 'responsible_user') {
+        return item.responsible_user?.name?.toLowerCase() ?? '';
+      }
+
+      const value = item[property as keyof Project];
+
+      return typeof value === 'number' || typeof value === 'string'
+        ? value
+        : '';
+    };
   }
 
   createProject() {
     const dialogRef = this.dialog.open(ProjectCreationFormComponent, {
       width: '600px',
     });
-
-    // dialogRef.afterOpened().subscribe(() => {
-    //   this.cdr.detectChanges(); // Manually trigger change detection
-    // });
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
